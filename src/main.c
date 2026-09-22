ead6ff54ed0e3f539a25d204d0cb5e29b5db3fa4
Michael Strommer <michael.strommer@fh-campuswien.ac.at>
2026-09-22 14:32:56 +0200

    Restore documented Hello World example

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/*
+ * Verwendung im Terminal:
+ *   make                         Programm bauen
+ *   make run                     Programm bauen und starten
+ *   make run ARGS="Hallo 123"   Programm mit Argumenten starten
+ *   make debug                   Programm mit GDB debuggen
+ *   make debug ARGS="Hallo 123" Programm mit Argumenten debuggen
+ *   make clean                   Erzeugte Dateien entfernen
+ */
+
 int main(void)
 {
     printf("Hello, World!\n");
